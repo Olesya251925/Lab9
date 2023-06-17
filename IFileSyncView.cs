ddfe4cce089fb5d32d5db7f0c2c02c3c03756600
Olesya251925 <118814113+Olesya251925@users.noreply.github.com>
2023-06-17 12:39:34 +0700

    Update IFileSyncView.cs

--- IFileSyncView.cs
+++ IFileSyncView.cs
@@ -2,9 +2,14 @@ using System;
 
 namespace FileSyncApp
 {
+    // Определение интерфейса IFileSyncView
     public interface IFileSyncView
     {
+        // Событие, возникающее при запросе на синхронизацию
         event EventHandler SyncRequested;
+
+        // Метод для отображения статуса синхронизации
         void DisplayStatus(string status);
     }
 }
+
